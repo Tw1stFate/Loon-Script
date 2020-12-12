316cb1f760885051b96480caf2f3152873a1cc98
lynn <devlynn@126.com>
2020-12-12 17:17:51 +0800

    m

--- Tasks_zzz/update_jd_code.js
+++ Tasks_zzz/update_jd_code.js
@@ -4,15 +4,28 @@
 //log
 console.log("开始添加京东助力码");
 
-//http get params
-// var params = {
-//     url:"http://api.turinglabs.net/api/v1/jd/farm/create/bc1cd851e963492884852dc6119c33a6/",
-//     header:{
-//         Host:"api.example.com",
-//         Content-Type: "application/json",
-//     },
-//     body:"string"
-// }
+const dict = [
+  {
+    "name":"农场",
+    "url":"http://api.turinglabs.net/api/v1/jd/farm/create/bc1cd851e963492884852dc6119c33a6/"
+  },
+  {
+    "name":"种豆",
+    "url":"http://api.turinglabs.net/api/v1/jd/bean/create/75f7c7vrcm5zbfod5t26fp44fi/"
+  },
+  {
+    "name":"宠萌",
+    "url":"http://api.turinglabs.net/api/v1/jd/pet/create/MTAxODEyMjkyMDAwMDAwMDQwMzg4ODEz/"
+  },
+  {
+    "name":"惊喜工厂",
+    "url":"http://api.turinglabs.net/api/v1/jd/jxfactory/create/Hz0ItUK3zR6NCRVu-ajkGg==/"
+  },
+  {
+    "name":"东东工厂",
+    "url":"http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5uK2s7ZWeqXuLg/"
+  },
+]
 
 !(async () => {
   await farm();
@@ -35,8 +48,8 @@ function farm() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/farm/create/bc1cd851e963492884852dc6119c33a6/",
       function (error, response, data) {
-        let massage = JSON.parse(data)
-        console.log('农场=>'+massage)
+        let data = JSON.parse(data)
+        console.log('农场=>data.massage')
         resolve();
       }
     );
@@ -48,8 +61,8 @@ function bean() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/bean/create/75f7c7vrcm5zbfod5t26fp44fi/",
       function (error, response, data) {
-        let massage = JSON.parse(data)
-        console.log('农场=>'+massage)
+        let data = JSON.parse(data)
+        console.log('农场=>data.massage')
 
         resolve();
       }
@@ -62,8 +75,8 @@ function pet() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/pet/create/MTAxODEyMjkyMDAwMDAwMDQwMzg4ODEz/",
       function (error, response, data) {
-        let massage = JSON.parse(data)
-        console.log('宠萌=>'+massage)
+        let data = JSON.parse(data)
+        console.log('宠萌=>data.massage')
         resolve();
       }
     );
@@ -75,8 +88,8 @@ function jxfactory() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/jxfactory/create/Hz0ItUK3zR6NCRVu-ajkGg==/",
       function (error, response, data) {
-        let massage = JSON.parse(data)
-        console.log('惊喜工厂=>'+massage)
+        let data = JSON.parse(data)
+        console.log('惊喜工厂=>data.massage')
         resolve();
       }
     );
@@ -88,8 +101,8 @@ function ddfactory() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5uK2s7ZWeqXuLg/",
       function (error, response, data) {
-        let massage = JSON.parse(data)
-        console.log('东东工厂=>'+massage)
+        let data = JSON.parse(data)
+        console.log('东东工厂=>data.massage')
         resolve();
       }
     );
