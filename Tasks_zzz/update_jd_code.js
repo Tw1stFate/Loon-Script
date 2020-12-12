175606b478bd6b361adf1fcfce8ed7d9471221ae
lynn <devlynn@126.com>
2020-12-12 17:21:23 +0800

    m

--- Tasks_zzz/update_jd_code.js
+++ Tasks_zzz/update_jd_code.js
@@ -35,11 +35,11 @@ console.log("=========开始重新添加京东助力码=========");
   await ddfactory();
 })()
   .catch((e) => {
-    this.log("", `\u2757\ufe0f${this.name}, \u9519\u8bef!`, t);
+    this.log("=========fail=========");
   })
   .finally(() => {
-    console.log('=========添加互助码success========')
-    $notification.post('重新添加互助码成功')
+    console.log("=========添加互助码success========");
+    $notification.post("重新添加互助码成功");
   });
 
 //京东农场
@@ -48,8 +48,8 @@ function farm() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/farm/create/bc1cd851e963492884852dc6119c33a6/",
       function (error, response, data) {
-        let data = JSON.parse(data)
-        console.log('农场=>data.massage')
+        let data = JSON.parse(data);
+        console.log("农场=>data.massage");
         resolve();
       }
     );
@@ -61,8 +61,8 @@ function bean() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/bean/create/75f7c7vrcm5zbfod5t26fp44fi/",
       function (error, response, data) {
-        let data = JSON.parse(data)
-        console.log('农场=>data.massage')
+        let data = JSON.parse(data);
+        console.log("农场=>data.massage");
 
         resolve();
       }
@@ -75,8 +75,8 @@ function pet() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/pet/create/MTAxODEyMjkyMDAwMDAwMDQwMzg4ODEz/",
       function (error, response, data) {
-        let data = JSON.parse(data)
-        console.log('宠萌=>data.massage')
+        let data = JSON.parse(data);
+        console.log("宠萌=>data.massage");
         resolve();
       }
     );
@@ -88,8 +88,8 @@ function jxfactory() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/jxfactory/create/Hz0ItUK3zR6NCRVu-ajkGg==/",
       function (error, response, data) {
-        let data = JSON.parse(data)
-        console.log('惊喜工厂=>data.massage')
+        let data = JSON.parse(data);
+        console.log("惊喜工厂=>data.massage");
         resolve();
       }
     );
@@ -101,8 +101,8 @@ function ddfactory() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5uK2s7ZWeqXuLg/",
       function (error, response, data) {
-        let data = JSON.parse(data)
-        console.log('东东工厂=>data.massage')
+        let data = JSON.parse(data);
+        console.log("东东工厂=>data.massage");
         resolve();
       }
     );
