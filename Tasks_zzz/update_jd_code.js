ea2db93d4a992015f993d2925ead1885fd82c028
lynn <devlynn@126.com>
2020-12-12 18:38:11 +0800

    m

--- Tasks_zzz/update_jd_code.js
+++ Tasks_zzz/update_jd_code.js
@@ -47,7 +47,7 @@ function farm() {
     $httpClient.get(
       "http://api.turinglabs.net/api/v1/jd/farm/create/bc1cd851e963492884852dc6119c33a6/",
       function (error, response, data) {
-        let resData = JSON.parse(data)
+        let resData = JSON.parse(data);
         console.log(`农场=> ${resData.message}`);
         resolve();
       }
@@ -55,54 +55,54 @@ function farm() {
   });
 }
 
-// function bean() {
-//   return new Promise((resolve, reject) => {
-//     $httpClient.get(
-//       "http://api.turinglabs.net/api/v1/jd/bean/create/75f7c7vrcm5zbfod5t26fp44fi/",
-//       function (error, response, data) {
-//         let res = JSON.parse(data);
-//         console.log("种豆=>"+res.massage);
-//         resolve();
-//       }
-//     );
-//   });
-// }
+function bean() {
+  return new Promise((resolve, reject) => {
+    $httpClient.get(
+      "http://api.turinglabs.net/api/v1/jd/bean/create/75f7c7vrcm5zbfod5t26fp44fi/",
+      function (error, response, data) {
+        let resData = JSON.parse(data);
+        console.log(`种豆=> ${resData.message}`);
+        resolve();
+      }
+    );
+  });
+}
 
-// function pet() {
-//   return new Promise((resolve, reject) => {
-//     $httpClient.get(
-//       "http://api.turinglabs.net/api/v1/jd/pet/create/MTAxODEyMjkyMDAwMDAwMDQwMzg4ODEz/",
-//       function (error, response, data) {
-//         let res = JSON.parse(data);
-//         console.log("宠萌=>"+res.massage);
-//         resolve();
-//       }
-//     );
-//   });
-// }
+function pet() {
+  return new Promise((resolve, reject) => {
+    $httpClient.get(
+      "http://api.turinglabs.net/api/v1/jd/pet/create/MTAxODEyMjkyMDAwMDAwMDQwMzg4ODEz/",
+      function (error, response, data) {
+        let resData = JSON.parse(data);
+        console.log(`宠萌=> ${resData.message}`);
+        resolve();
+      }
+    );
+  });
+}
 
-// function jxfactory() {
-//   return new Promise((resolve, reject) => {
-//     $httpClient.get(
-//       "http://api.turinglabs.net/api/v1/jd/jxfactory/create/Hz0ItUK3zR6NCRVu-ajkGg==/",
-//       function (error, response, data) {
-//         let res = JSON.parse(data);
-//         console.log("惊喜工厂=>"+res.massage);
-//         resolve();
-//       }
-//     );
-//   });
-// }
+function jxfactory() {
+  return new Promise((resolve, reject) => {
+    $httpClient.get(
+      "http://api.turinglabs.net/api/v1/jd/jxfactory/create/Hz0ItUK3zR6NCRVu-ajkGg==/",
+      function (error, response, data) {
+        let resData = JSON.parse(data);
+        console.log(`惊喜工厂=> ${resData.message}`);
+        resolve();
+      }
+    );
+  });
+}
 
-// function ddfactory() {
-//   return new Promise((resolve, reject) => {
-//     $httpClient.get(
-//       "http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5uK2s7ZWeqXuLg/",
-//       function (error, response, data) {
-//         let res = JSON.parse(data);
-//         console.log("东东工厂=>"+res.massage);
-//         resolve();
-//       }
-//     );
-//   });
-// }
+function ddfactory() {
+  return new Promise((resolve, reject) => {
+    $httpClient.get(
+      "http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5uK2s7ZWeqXuLg/",
+      function (error, response, data) {
+        let resData = JSON.parse(data);
+        console.log(`东东工厂=> ${resData.message}`);
+        resolve();
+      }
+    );
+  });
+}
